76d2caa90c97d154c3a076db28181fe093e1dce4
MoisesGJ <mgarciaj1802@alumno.ipn.mx>
2024-02-19 18:16:13 -0600

    Fix share with ig

--- src/components/Messages/index.jsx
+++ src/components/Messages/index.jsx
@@ -9,15 +9,12 @@ export default function Messages({ id, author, message, date }) {
         const imageurl = canvas.toDataURL('image/jpeg');
 
         saveAs(imageurl, `image${id}.jpg`);
-
-        const timer = setTimeout(() => {
-          window.location.href = `instagram://library?OpenInEditor=1&LocalIdentifier=+1`;
-        }, 3000);
-        return () => clearTimeout(timer);
       })
       .catch((error) => console.error(error));
   };
 
+  const handlerGoToIg = () =>
+    (window.location.href = `instagram://library?OpenInEditor=1&LocalIdentifier=+1`);
   return (
     <div
       className="relative flex flex-col mt-6 text-sm text-gray-900 bg-gray-50 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 shadow-md bg-clip-border rounded-xl w-full"
@@ -34,13 +31,26 @@ export default function Messages({ id, author, message, date }) {
           {date}
         </span>
       </div>
-      <div className="p-6 pt-0">
+      <div className="p-6 pt-0 flex items-center gap-5">
         <button
           className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
           type="button"
           onClick={handlerSaveImage}
         >
-          Compartir
+          Guardar
+        </button>
+        <button onClick={handlerGoToIg}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="white"
+              d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"
+            />
+          </svg>
         </button>
       </div>
     </div>
